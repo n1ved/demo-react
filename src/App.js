9eb18b12344506e4d97fb48d1fc659ca1cf1d949
n1ved <nivedrsalini@gmail.com>
2023-11-25 15:15:42 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const result = await fetch('http://127.0.0.1:8000/');
+      setData(result.data);
+      setLoading(false);
+    };
+
+    fetchData();
+    console.log(data);
+  }, []);
+
+  const sendRequest = async () => {
+    const response = await fetch('http://127.0.0.1/8000/' , {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ data: 'Hello' }),
+    });
+    console.log(response);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading ? 'Loading...' : data}
+      <button onClick={sendRequest}>Send Request</button>
     </div>
   );
 }
